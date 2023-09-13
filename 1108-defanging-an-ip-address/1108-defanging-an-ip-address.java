1a705a769faee74963ed9b6b04eff52d3bb878e1
Muhammad Husni Nur Fadillah <muhammadhusninurfadillah@gmail.com>
2023-09-13 08:46:39 +0800

    Time: 0 ms (100.00%), Space: 40.4 MB (57.30%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -1,9 +1,5 @@
 class Solution {
     public String defangIPaddr(String address) {
-        StringBuilder str = new StringBuilder();
-        for (char c : address.toCharArray()){
-            str.append((c == '.')? "[.]" : c );
-        }
-        return str.toString();
+        return address.replace("." , "[.]");
     }
 }
